eb34766beb86a14fb2cd6fba2a0b388d81e044e9
Linda Chang <lindachang186@gmail.com>
2018-10-12 02:25:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,13 @@
 def my_collect(collection)
   i = 0
+  new = []
   while i < collection.size
-    yield collection[i]
+    yield new.push(collection[i])
     i += 1
   end
   return collection
+end
+
+my_collect(collection).map do |name|
+  puts name.capitalize
 end
