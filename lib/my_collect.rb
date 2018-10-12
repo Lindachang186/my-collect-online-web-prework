8e4e1cbe190bc841fb251376424e8a543ebbd2a2
Linda Chang <lindachang186@gmail.com>
2018-10-12 02:13:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,7 @@
 def my_collect(collection)
   i = 0
-  while collection.size < i
-    yield
+  while i < collection.size
+    yield collection[i]
     i += 1
   end
-end
-
-my_collect(languages)
-  languages.map do |name|
-    name.capitalizes
-  end
-end
 end
