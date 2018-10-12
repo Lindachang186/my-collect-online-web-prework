94845ce0f6a5d4dfa89d45b897460f0f7ededec8
Linda Chang <lindachang186@gmail.com>
2018-10-12 01:58:18 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,13 @@
 def my_collect(collection)
-  i = 0 
+  i = 0
   while collection.size < i
     yield
     i += 1
   end
 end
+
+yielding(collection) do |name|
+  my_collect(array) do |name|
+    name.split(" ").first
+  end
+end 
