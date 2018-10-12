be7b25bb2025749f6af2a7b0c824aba4ccb884e5
Linda Chang <lindachang186@gmail.com>
2018-10-12 02:52:54 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,11 +11,6 @@ end
 def my_collect(students)
   students.collect do |student|
   student.split(" ").first
+  student.upcase
 end
-end
-
-def my_collect(languages)
-  languages.collect do |language|
-    language.upcase
-  end
 end
